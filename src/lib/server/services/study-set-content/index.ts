2c51f2581f85f1ca7f652d6f4e9476c55f00b128
falentio <kfalentio@proton.me>
2026-06-03 21:59:21 +0700

    feat(study-set-content): wire singletons in index.ts

---
+++ src/lib/server/services/study-set-content/index.ts
@@ -0,0 +1,11 @@
+import { StudySetContentDrizzleRepository } from './study-set-content.repository.drizzle.ts';
+import { StudySetContentGuard } from './study-set-content.guard.ts';
+import { StudySetContentService } from './study-set-content.service.ts';
+import { studySetGuard } from '../study-set/index.ts';
+
+const studySetContentRepo = new StudySetContentDrizzleRepository();
+export const studySetContentGuard = new StudySetContentGuard(studySetContentRepo, studySetGuard);
+export const studySetContentService = new StudySetContentService(
+	studySetContentRepo,
+	studySetContentGuard
+);
