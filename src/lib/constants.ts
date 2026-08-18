a86a397a75b26d97a5f2d5d32c6be7a1582b8f6b
falentio <kfalentio@proton.me>
2026-08-18 19:30:47 +0700

    chore: update instagram handle

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const EMAIL_SUPPORT = "support@sinnau.com";
-export const INSTAGRAM_URL = "https://www.instagram.com/sinnau.com/";
-export const INSTAGRAM_HANDLE = "@sinnau.com";
+export const INSTAGRAM_URL = "https://www.instagram.com/sinnau_/";
+export const INSTAGRAM_HANDLE = "@sinnau_";
